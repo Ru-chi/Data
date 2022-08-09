4fe4898afb4d6abeb953d86d32e97e8f2dcb66f8
Ru-chi <110914618+Ru-chi@users.noreply.github.com>
2022-08-09 09:17:12 -0500

    Add files via upload

---
+++ stream.py
@@ -0,0 +1,98 @@
+import tweepy
+import socket
+import re
+import preprocessor
+
+# Enter your Twitter keys here!!!
+ACCESS_TOKEN = '1392142676234014720-9TJ4kLh2cASpzWinuYYH7Kb7HslZwy'
+ACCESS_SECRET = 'lx9pkhcHOxEdRPUbIzSnqfR28n6thukQA0B95oefHBrpm'
+CONSUMER_KEY = 'QLLu2rJg6PZEAV1o9a1pbDGSa'
+CONSUMER_SECRET = 'ntB1QeL0LHYKoGBgIwsSf16sLvf3Zu5QDaioZF9i5Filfn3Zzf'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+#can set multiple hashtags and set whatever 
+hashtag = '#covid19'
+
+TCP_IP = 'localhost'
+TCP_PORT = 9001
+
+
+
+def preprocessing(tweet):
+    
+    # Add here your code to preprocess the tweets and  
+
+    emoji = re.compile(pattern = "["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002702-\U000027B0" #
+        u"\U000024C2-\U0001F251" #
+        "]+", flags=re.UNICODE)
+
+    tweet = re.sub(r'[^\x00-\x7F]+',' ',tweet)
+    tweet = emoji.sub(r'',tweet)
+    return preprocessor.clean(tweet)
+
+
+
+def getTweet(status):
+    
+    # You can explore fields/data other than location and the tweet itself. 
+    # Check what else you could explore in terms of data inside Status object
+
+    tweet = ""
+    location = ""
+
+    location = status.user.location
+    
+    if hasattr(status, "retweeted_status"):  # Check if Retweet
+        try:
+            tweet = status.retweeted_status.extended_tweet["full_text"]
+        except AttributeError:
+            tweet = status.retweeted_status.text
+    else:
+        try:
+            tweet = status.extended_tweet["full_text"]
+        except AttributeError:
+            tweet = status.text
+
+    return location, preprocessing(tweet)
+
+
+
+
+
+# create sockets
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+conn, addr = s.accept()
+
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        location, tweet = getTweet(status)
+
+        if (location != None and tweet != None):
+            tweetLocation = location + "::" + tweet+"\n"
+            #code added
+            print(status.text)
+            conn.send(tweetLocation.encode('utf-8'))
+
+        return True
+
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            return False
+        else:
+            print(status_code)
+
+myStream = tweepy.Stream(auth=auth, listener=MyStreamListener())
+myStream.filter(track=[hashtag], languages=["en"])
+
+
